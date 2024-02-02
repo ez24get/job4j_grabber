4c4903e036eb9af1d0ccfdd535567a0e10716629
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-02-02 15:05:43 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quarz/AlertRabbit.java
+++ src/main/java/ru/job4j/quarz/AlertRabbit.java
@@ -4,7 +4,9 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.InputStream;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -12,6 +14,17 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+
+    private Connection connection;
+
+    public AlertRabbit() {
+        init();
+    }
+
+    public AlertRabbit(Connection connection) {
+        this.connection = connection;
+    }
+
     public static void main(String[] args) {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
@@ -32,12 +45,37 @@ public class AlertRabbit {
 
     public static int getProperties() {
         try (InputStream input = AlertRabbit.class.getClassLoader()
-                .getResourceAsStream("rabbit.properties")) {
+                .getResourceAsStream("db/rabbit.properties")) {
             Properties config = new Properties();
             config.load(input);
             return Integer.parseInt(config.getProperty("rabbit.interval"));
         } catch (Exception e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    private void init() {
+        try (InputStream input = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("db/rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(input);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void createTable() {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("CREATE TABLE IF NOT EXISTS rabbit created TIMESTAMP;")) {
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
