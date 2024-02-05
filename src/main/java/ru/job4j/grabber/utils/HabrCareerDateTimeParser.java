6037185df13a87b067ed6b2cbb057457edf66aa4
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-02-05 13:35:59 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -0,0 +1,12 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HabrCareerDateTimeParser implements DateTimeParser {
+    @Override
+    public LocalDateTime parse(String parse) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        return LocalDateTime.parse(parse, formatter);
+    }
+}
---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    public void parse() {
+        String str = "2024-02-05T11:28:38+03:00";
+        String result = "05-февраля-понедельник-2024 11:28:38";
+        HabrCareerDateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        LocalDateTime localDateTime = dateTimeParser.parse(str);
+        assertThat(localDateTime.toString().equals(result)).isTrue();
+    }
+}
