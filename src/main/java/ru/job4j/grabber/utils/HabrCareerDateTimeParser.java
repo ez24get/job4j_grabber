099324bcb48392ef44b5095dff6a049ae88465df
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-02-06 14:42:32 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -3,10 +3,13 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
+        LocalDateTime dateTime = LocalDateTime.parse(parse, ISO_LOCAL_DATE_TIME);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        return LocalDateTime.parse(parse, formatter);
+        return LocalDateTime.parse(dateTime.format(formatter));
     }
 }
