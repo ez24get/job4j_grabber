b3ae24a5154287470d056decc2b6083b8a655a4c
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-02-10 17:00:05 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,42 +5,69 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
+    private final  DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     private String retrieveDescription(String link) throws IOException {
         Connection connection = Jsoup.connect(link);
         Document document = connection.get();
         Elements descriptionBody = document.select(".vacancy-description__text");
-        String body = Jsoup.parse(descriptionBody.html()).wholeText();
-        System.out.println(body);
-        return body;
+        return Jsoup.parse(descriptionBody.html()).wholeText();
     }
 
-    public static void main(String[] args) throws IOException {
+    private List<Post> createPost(String link) throws IOException {
+        List<Post> result = new ArrayList<>();
         for (int pageNumber = 1; pageNumber < 6; pageNumber++) {
-            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            DateTimeParser dateTimeParse = new HabrCareerDateTimeParser();
+            Post post = new Post();
+            String fullLink = "%s%s%d%s".formatted(link, PREFIX, pageNumber, SUFFIX);
             Connection connection = Jsoup.connect(fullLink);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
                 Element date = row.select(".vacancy-card__date").first();
                 Element vacancyDate = date.child(0);
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s Опубликовано - %s%n",
-                        vacancyName,
-                        link,
-                        vacancyDate.attr("datetime"));
+                String dateTime = vacancyDate.attr("datetime");
+                String linkOut = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                post.setTitle(titleElement.text());
+                post.setLink(linkOut);
+                post.setCreated(dateTimeParse.parse(dateTime));
+                try {
+                    post.setDescription(retrieveDescription(linkOut));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                result.add(post);
             });
         }
+        return result;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> result = new ArrayList<>();
+        try {
+            result.addAll(createPost(link));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
