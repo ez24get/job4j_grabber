be01eb47d387c50dc9176f05d1e7180ce31cd163
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-02-14 08:00:03 +0300

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -1,5 +1,7 @@
 package ru.job4j.grabber;
 
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 import ru.job4j.quarz.AlertRabbit;
 
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.io.InputStream;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -26,7 +29,7 @@ public class PsqlStore implements Store {
                 config.getProperty("password"));
     }
 
-    private Properties readProperties() {
+    private static Properties readProperties() {
         Properties properties = new Properties();
         try (InputStream in = AlertRabbit.class.getClassLoader().
                 getResourceAsStream("db/grabber/grabber.properties")) {
@@ -42,6 +45,7 @@ public class PsqlStore implements Store {
                 resultSet.getInt("id"),
                 resultSet.getString("name"),
                 resultSet.getString("description"),
+                resultSet.getString("link"),
                 resultSet.getTimestamp("created").toLocalDateTime()
         );
     }
@@ -50,7 +54,10 @@ public class PsqlStore implements Store {
     public void save(Post post) {
         try (PreparedStatement statement =
                      connection.prepareStatement(
-                             "INSERT INTO post(name, description, link, created) VALUES (?, ?, ?, ?)")) {
+                             "INSERT INTO post(name, description, link, created)"
+                                     + " VALUES (?, ?, ?, ?)"
+                                     + " ON CONFLICT (link)"
+                                     + " DO NOTHING;")) {
             statement.setString(1, post.getTitle());
             statement.setString(2, post.getDescription());
             statement.setString(3, post.getLink());
@@ -81,7 +88,8 @@ public class PsqlStore implements Store {
     public Post findById(int id) {
         Post post = new Post();
         try (PreparedStatement statement =
-                     connection.prepareStatement("SELECT id, name, description, link, created FROM items WHERE id = ?")) {
+                     connection.prepareStatement("SELECT id, name, description, link, created"
+                             + " FROM items WHERE id = ?")) {
             statement.setInt(1, id);
             statement.execute();
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -101,6 +109,23 @@ public class PsqlStore implements Store {
     public void close() throws Exception {
         if (connection != null) {
             connection.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Post post = new Post();
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        LocalDateTime time = dateTimeParser.parse("2007-12-03T10:15:30+01:00");
+        post.setTitle("Java");
+        post.setDescription("bla-bla");
+        post.setLink("link");
+        post.setCreated(time);
+        Properties properties = readProperties();
+        try (PsqlStore psqlPost = new PsqlStore(properties)) {
+            psqlPost.save(post);
+            System.out.println(psqlPost.getAll());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
