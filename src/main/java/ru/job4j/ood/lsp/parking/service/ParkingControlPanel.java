9a79d966eebf81c7283142014b4e3b4ac08853d4
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-04-18 09:12:18 +0300

    2. Парковка машин [#853 #490773]

--- src/main/java/ru/job4j/ood/lsp/parking/service/ParkingControlPanel.java
+++ src/main/java/ru/job4j/ood/lsp/parking/service/ParkingControlPanel.java
@@ -1,4 +1,19 @@
 package ru.job4j.ood.lsp.parking.service;
 
+import ru.job4j.ood.lsp.parking.model.Car;
+
+import java.util.Map;
+
 public class ParkingControlPanel {
+    private Map<String, Integer> car;
+
+    public ParkingControlPanel(Map<String, Integer> car) {
+        this.car = car;
+    }
+
+    public void parkTo(Map<String, Integer> car) {
+    }
+
+    public void leave(Map<String, Integer> car) {
+    }
 }
