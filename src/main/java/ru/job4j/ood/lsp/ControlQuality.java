675a44cd41100cb42d5aad5ac51b36f75735e26f
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-04-27 16:28:06 +0300

    1. Динамическое перераспределение продуктов [#854]

--- src/main/java/ru/job4j/ood/lsp/ControlQuality.java
+++ src/main/java/ru/job4j/ood/lsp/ControlQuality.java
@@ -6,6 +6,8 @@ import ru.job4j.ood.lsp.storage.Store;
 import ru.job4j.ood.lsp.storage.Trash;
 import ru.job4j.ood.lsp.storage.Warehouse;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ControlQuality {
@@ -18,6 +20,20 @@ public class ControlQuality {
     public void distribute(List<Food> foodList) {
         for (Store s : list) {
             for (Food food : foodList) {
+                if (s.accept(food)) {
+                    s.add(food);
+                }
+            }
+        }
+    }
+
+    public void resort() {
+        List<Food> foodListResort = new ArrayList<>();
+        for (Store s : list) {
+            foodListResort.addAll(s.getList());
+        }
+        for (Store s : list) {
+            for (Food food : foodListResort) {
                 if (s.accept(food)) {
                     s.add(food);
                 }
